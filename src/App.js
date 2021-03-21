cda87ee24e58df7007438732136a3754cf2ff524
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-03-21 18:21:02 +0500

    added header on dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import Dashboard from "./Pages/Dashboard/dashboard";
 import Landing from "./Pages/LandingPage/landing";
 
 function App() {
   return (
     <Fragment>
-        <Landing/>
+        {/* <Landing/> */}
+        <Dashboard/>
     </Fragment>
   );
 }
---
+++ src/Pages/Dashboard/dashboard.js
@@ -0,0 +1,13 @@
+import react,{Fragment} from 'react';
+import Header from '../../Components/Header/header'
+
+
+function Dashboard(props) {
+    return (
+      <Fragment>
+          <Header/>
+      </Fragment>
+    )
+}
+
+export default Dashboard
