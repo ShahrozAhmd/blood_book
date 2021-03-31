d889a4e8a209d6f60093194a690b2a020eae5c67
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-03-31 10:51:53 +0500

    imports optimization for header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Badge,
+  MenuItem,
+  Menu,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
+  // icon: {
+  //  backgroundColor: "#17d9f7",
+  // backgroundImage: "linear-gradient(90deg, #17d9f7 0%, #4e21ff 100%)",
+
+  // },
+  appbar: {
+    backgroundColor: "#17d9f7",
+    backgroundImage: "linear-gradient(90deg, #17d9f7 0%, #4e21ff 100%)",
+  },
   grow: {
     flexGrow: 1,
   },
@@ -23,58 +34,58 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: 'auto',
+      width: "auto",
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
   },
   sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
   },
 }));
@@ -104,14 +115,14 @@ const Header = (props) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -120,19 +131,22 @@ const Header = (props) => {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
+        <IconButton
+          style={{ backgroundColor: "green" }}
+          aria-label="show 4 new mails"
+        >
           <Badge badgeContent={4} color="secondary">
             <MailIcon />
           </Badge>
@@ -163,7 +177,7 @@ const Header = (props) => {
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appbar}>
         <Toolbar>
           <IconButton
             edge="start"
@@ -174,7 +188,7 @@ const Header = (props) => {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Material-UI
+            Blood Book
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
@@ -186,7 +200,7 @@ const Header = (props) => {
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </div>
           <div className={classes.grow} />
@@ -229,6 +243,6 @@ const Header = (props) => {
       {renderMenu}
     </div>
   );
-}
+};
 
 export default Header;
--- src/Pages/Dashboard/dashboard.js
+++ src/Pages/Dashboard/dashboard.js
@@ -1,13 +1,62 @@
-import react,{Fragment} from 'react';
-import Header from '../../Components/Header/header'
+import { Box, Container, Grid, makeStyles } from "@material-ui/core";
+import { Height } from "@material-ui/icons";
+import react, { Fragment } from "react";
+import Header from "../../Components/Header/header";
 
+const useStyles = makeStyles({
+  box: {
+    border: "1px solid black",
+  },
+  
+    container: {
+      height: "100vh",
+      backgroundColor: "white",
+    },
+
+  sec1: {
+    width: "25%",
+    height: "100vh",
+    border: "1px solid black",
+  },
+  sec2: {
+    width: "50%",
+    height: "100vh",
+    border: "1px solid black",
+  },
+  sec3: {
+    width: "25%",
+    height: "100vh",
+    border: "1px solid black",
+  },
+});
 
 function Dashboard(props) {
-    return (
-      <Fragment>
-          <Header/>
-      </Fragment>
-    )
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Container disableGutters maxWidth={1}>
+        <Header />
+
+        <Grid container>
+          <Grid item>
+            <Box className={classes.sec1}>
+              <p>1</p>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Box className={classes.sec2}>
+              <p>2</p>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Box className={classes.sec3}>
+              <p>3</p>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
