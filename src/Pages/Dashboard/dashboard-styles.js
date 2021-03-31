0c93d2cc1d74ee1993a70c68d05f0048b7ee727c
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-03-31 10:57:24 +0500

    make a separate style file for dashboard.js

---
+++ src/Pages/Dashboard/dashboard-styles.js
@@ -0,0 +1,16 @@
+import {makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles({
+    box: {
+      border: "1px solid black",
+    },
+    
+      container: {
+        height: "100vh",
+        backgroundColor: "white",
+      },
+  });
+
+  export default useStyles;
+  
--- src/Pages/Dashboard/dashboard.js
+++ src/Pages/Dashboard/dashboard.js
@@ -1,34 +1,10 @@
-import { Box, Container, Grid, makeStyles } from "@material-ui/core";
+import { Box, Container, Grid} from "@material-ui/core";
 import { Height } from "@material-ui/icons";
 import react, { Fragment } from "react";
 import Header from "../../Components/Header/header";
+import useStyles from './dashboard-styles'
 
-const useStyles = makeStyles({
-  box: {
-    border: "1px solid black",
-  },
-  
-    container: {
-      height: "100vh",
-      backgroundColor: "white",
-    },
 
-  sec1: {
-    width: "25%",
-    height: "100vh",
-    border: "1px solid black",
-  },
-  sec2: {
-    width: "50%",
-    height: "100vh",
-    border: "1px solid black",
-  },
-  sec3: {
-    width: "25%",
-    height: "100vh",
-    border: "1px solid black",
-  },
-});
 
 function Dashboard(props) {
   const classes = useStyles();
@@ -39,19 +15,13 @@ function Dashboard(props) {
 
         <Grid container>
           <Grid item>
-            <Box className={classes.sec1}>
-              <p>1</p>
-            </Box>
+           
           </Grid>
           <Grid item>
-            <Box className={classes.sec2}>
-              <p>2</p>
-            </Box>
+            
           </Grid>
           <Grid item>
-            <Box className={classes.sec3}>
-              <p>3</p>
-            </Box>
+            
           </Grid>
         </Grid>
       </Container>
