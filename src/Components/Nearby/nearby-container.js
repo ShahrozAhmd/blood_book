621ce16c86e22e2475ce61d95d5556871284e748
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-04-14 00:03:57 +0500

    setup component structure for nearby section

---
+++ src/Components/Nearby/nearby-container.js
@@ -0,0 +1,11 @@
+import react from "react";
+import Nearby from "./nearby";
+import useStyles from "./nearby-styles";
+
+const NearbyContainer = (props) => {
+  const classes = useStyles();
+
+  return <div></div>;
+};
+
+export default NearbyContainer;
---
+++ src/Components/Nearby/nearby.js
@@ -0,0 +1,10 @@
+import react from "react";
+import useStyles from "./nearby-styles";
+
+const Nearby = (props) => {
+  const classes = useStyles();
+
+  return <div></div>;
+};
+
+export default Nearby;
