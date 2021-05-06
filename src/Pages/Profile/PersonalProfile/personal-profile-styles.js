847576517c5665786ec61a526ce1a62ac8544915
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-05-07 00:58:22 +0500

    structure has been setup for general profile

---
+++ src/Pages/Profile/PersonalProfile/personal-profile-styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(1),
+    width: "100%",
+    height: "95%",
+  },
+  container: {
+    border: "1px solid black",
+    height: "100vh",
+    maxHeight: "100vh",
+  },
+}));
+
+export default useStyles;
--- src/Pages/Profile/PersonalProfile/personal-profile.js
+++ src/Pages/Profile/PersonalProfile/personal-profile.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useStyles from "./personal-profile-styles";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 function PersonalProfile(props) {
-    return (
-        <div>
-            have to desig a page which showcase the profile as well as edit it,
-            <br/>
-            now the question is how it looks and feel ????
-            
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    // <div className={classes.root}>
+    // </div>
+
+    <Fragment>
+      <CssBaseline />
+      <Container maxWidth="md" className = {classes.container} >
+        <Paper elevation={3} className={classes.paper}/>
+      </Container>
+    </Fragment>
+  );
 }
 
-export default PersonalProfile
-
-
+export default PersonalProfile;
