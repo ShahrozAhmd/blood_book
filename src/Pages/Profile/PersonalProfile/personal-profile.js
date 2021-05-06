e2f1e5eef9ee3e05776789ed7fb63089e86c142b
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-05-06 19:27:12 +0500

    added some files

---
+++ src/Pages/Profile/PersonalProfile/personal-profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function PersonalProfile(props) {
+    return (
+        <div>
+            have to desig a page which showcase the profile as well as edit it,
+            <br/>
+            now the question is how it looks and feel ????
+            
+        </div>
+    )
+}
+
+export default PersonalProfile
+
+
--- src/Pages/Profile/profile.js
+++ src/Pages/Profile/profile.js
@@ -4,31 +4,33 @@ import Paper from "@material-ui/core/Paper";
 import { Box, Typography } from "@material-ui/core";
 import AccessibleIcon from "@material-ui/icons/Accessible";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import PersonalProfile from "./PersonalProfile/personal-profile";
 
 function Profile(props) {
   const classes = useStyles();
   return (
-    <Box
-      className={classes.root}
-      display="flex"
-      flexWrap="wrap"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Paper elevation={3} className={classes.card}>
-        <Box className={classes.btnBox}>
-          <PersonAddIcon />
-          <Typography variant="p">General</Typography>
-        </Box>
-      </Paper>
+    <PersonalProfile />
+    // <Box
+    //   className={classes.root}
+    //   display="flex"
+    //   flexWrap="wrap"
+    //   justifyContent="center"
+    //   alignItems="center"
+    // >
+    //   <Paper elevation={3} className={classes.card}>
+    //     <Box className={classes.btnBox}>
+    //       <PersonAddIcon />
+    //       <Typography variant="p">General</Typography>
+    //     </Box>
+    //   </Paper>
 
-      <Paper elevation={3} className={classes.card}>
-        <Box className={classes.btnBox}>
-          <AccessibleIcon />
-          <Typography variant="p">Medical</Typography>
-        </Box>
-      </Paper>
-    </Box>
+    //   <Paper elevation={3} className={classes.card}>
+    //     <Box className={classes.btnBox}>
+    //       <AccessibleIcon />
+    //       <Typography variant="p">Medical</Typography>
+    //     </Box>
+    //   </Paper>
+    // </Box>
   );
 }
 
