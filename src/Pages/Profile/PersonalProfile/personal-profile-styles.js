b21c06d3e988109c0568b45fe50c63aa25ed38f7
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-05-07 20:07:20 +0500

    section 01 structure of profile page has been added

--- src/Pages/Profile/PersonalProfile/personal-profile-styles.js
+++ src/Pages/Profile/PersonalProfile/personal-profile-styles.js
@@ -4,12 +4,13 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     margin: theme.spacing(1),
     width: "100%",
-    height: "95%",
+    height: "100vh",
   },
   container: {
     border: "1px solid black",
     height: "100vh",
     maxHeight: "100vh",
+    overflow:"scroll"
   },
   el: {
     border: "1px solid grey",
--- src/Pages/Profile/PersonalProfile/personal-profile.js
+++ src/Pages/Profile/PersonalProfile/personal-profile.js
@@ -8,13 +8,22 @@ import { Box } from "@material-ui/core";
 function PersonalProfile(props) {
   const classes = useStyles();
   return (
-    // <div className={classes.root}>
-    // </div>
-
     <Fragment>
       <CssBaseline />
       <Container maxWidth="md" className={classes.container}>
         <Paper elevation={3} className={classes.paper}>
+          {/* SECTION 01 STARTS HERE */}
+          <Box
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="space-between"
+            style={{ border: "1px solid black", padding: "1%" }}
+          >
+            <Box className={classes.el}>Profile Complete</Box>
+            <Box className={classes.el}>Three dots</Box>
+          </Box>
+          {/* SECTION 01 ENDS HERE */}
+
           {/* SECTION 02 STARTS HERE */}
           <Box
             display="flex"
