5a5e9d1c09c11d4812051b85a2508fe5415c0443
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-05-21 20:48:51 +0500

    add some code

--- src/Components/EligibileDonorForm/Eligibile-donor-form.js
+++ src/Components/EligibileDonorForm/Eligibile-donor-form.js
@@ -1,12 +1,34 @@
 import React from "react";
-import useStyles from './Eligibile-donor-form-styles'
+// import useStyles from './Eligibile-donor-form-styles'
+import Typography from "@material-ui/core/Typography";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+const BorderLinearProgress = withStyles((theme) => ({
+  root: {
+    height: 10,
+    borderRadius: 5,
+  },
+  colorPrimary: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
+  },
+  bar: {
+    borderRadius: 5,
+    backgroundColor: "#1a90ff",
+  },
+}))(LinearProgress);
 
 function EligibileDonorForm(props) {
-    const classes = useStyles();
-    
+  // const classes = useStyles();
+
   return (
     <div>
-      <p>Hello World</p>
+      <BorderLinearProgress variant="determinate" value={20} />
+      <Typography variant="subtitle1" gutterBottom>
+        Its a quick test, If you have any of these Diseases, you are not Fine to
+        donate blood to anyone, must Check the diseases you have.
+      </Typography>
     </div>
   );
 }
