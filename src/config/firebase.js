53b8cbaf7fbb1296b04da4e78841273397f4bb81
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-06-09 19:37:23 +0500

    added firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyABDsZz3zthfTvAOAt5AB0oYCxyWYM1ag4",
+    authDomain: "bloodbook-2eab5.firebaseapp.com",
+    projectId: "bloodbook-2eab5",
+    storageBucket: "bloodbook-2eab5.appspot.com",
+    messagingSenderId: "133539561230",
+    appId: "1:133539561230:web:71075587202275b729615f",
+    measurementId: "G-20JEKXPMG2"
+  };
